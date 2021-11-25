d341fdca249342d43c2d50f9d5c5ad219fa832d0
Krys <75592214+Kooties@users.noreply.github.com>
2021-11-24 16:49:31 -0800

    cREATION

---
+++ fonts_installer.ps1
@@ -0,0 +1,9 @@
+$fontFolder = "C:\folder\with\fonts\in\it"
+$FontItem = Get-Item -Path $FontFolder
+$FontList = Get-ChildItem -Path "$FontItem\*" -Include ('*.fon','*.otf','*.ttc','*.ttf')
+
+foreach ($Font in $FontList) {
+    Copy-Item $Font "C:\Windows\Fonts"
+    New-ItemProperty -Name $Font.BaseName -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Fonts" -PropertyType string -Value $Font.name         
+}
+
