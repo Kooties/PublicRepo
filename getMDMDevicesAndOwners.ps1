af577640bfcc81b80642a2370c023f53f4e2f884
Krys <75592214+Kooties@users.noreply.github.com>
2022-07-07 13:47:09 -0400

    updates to MDMDevicesAndOwners.ps1

--- getMDMDevicesAndOwners.ps1
+++ getMDMDevicesAndOwners.ps1
@@ -2,19 +2,24 @@
 Krystina Kyle, 2022#>
 
 Connect-AzureAD
-
-$devices = Get-AzureADDevice | Where-Object {($_.DeviceOSType -notlike "Windows") -and ($_.IsManaged -eq $true)}
+$devices = Get-AzureADDevice | Where-Object {($_.DeviceOSType -notlike "Windows") -and ($_.IsManaged -eq $true) -and ($_.IsCompliant -eq $true)}
 $ownersAndDevices = New-Object System.Data.DataTable
 [void]$ownersAndDevices.Columns.Add("Display Name")
 [void]$ownersAndDevices.Columns.Add("Email Address")
 [void]$ownersAndDevices.Columns.Add("Device Type")
-[void]$ownersAndDevices.Columns.Add("Is Managed")
-[void]$ownersAndDevices.Columns.Add("Is Compliant")
 
 foreach($device in $devices){
     $owner = Get-AzureADDeviceRegisteredOwner -ObjectId $device.ObjectId
-    [void]$ownersAndDevices.Rows.Add($owner.DisplayName, $owner.UserPrincipalName, $device.DeviceOSType, $device.IsManaged, $device.IsCompliant)
+    if($device.DeviceOSType -like "AndroidForWork"){
+        $os = "Android"
+    }elseif($device.DeviceOSType -like "IPhone"){
+        $os = "iOS"
+    }
+    [void]$ownersAndDevices.Rows.Add($owner.DisplayName, $owner.UserPrincipalName, $os)
 }
 
-return $ownersAndDevices
+#return $ownersAndDevices
+
+$ownersAndDevices | Export-CSV -Path .\MDMDevicesandOwners.csv
+
 $ownersAndDevices.clear()
