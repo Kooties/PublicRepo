a5e48a6e0952976d8b16e6404e272eab210b082c
Krys <75592214+Kooties@users.noreply.github.com>
2023-05-09 14:27:54 -0400

    fix

--- IntuneRemediations/LAPS Prep/Local Guest/BuiltInGuestRename_Detection.ps1
+++ IntuneRemediations/LAPS Prep/Local Guest/BuiltInGuestRename_Detection.ps1
@@ -2,6 +2,6 @@ $accname = "whateveryounamedit"
 
 $acc = Get-LocalUser $accname
 
-if($accname.sid -match "S-1-5-21-\d{10}-\d{10}-\d{10}-501"){
+if($acc.sid -match "S-1-5-21-\d{10}-\d{10}-\d{10}-501"){
     return 1;
 }
