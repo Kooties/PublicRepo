f25f0d6ff5fee8381c9a2cc5c4080c6f88a13ce3
Krystina Kyle <krystina.kyle@storable.com>
2024-06-03 11:50:05 -0400

    update

--- BlogScripts/GCPW_Install/uninstallGCPW.ps1
+++ BlogScripts/GCPW_Install/uninstallGCPW.ps1
@@ -1,1 +1,7 @@
+Remove-Item -Path HKLM:\SOFTWARE\Google\GCPW -Force
+Remove-Item -Path 'HKLM:\SOFTWARE\WOW6432Node\Google\Update\ClientState\{430FD4D0-B729-4F61-AA34-91526481799D}' -Force
+Remove-Item -Path 'HKLM:\SOFTWARE\WOW6432Node\Google\Update\ClientState\{32987697-A14E-4B89-84D6-630D5431E831}' -Force
+
+
+
 .\gcpwstandaloneenterprise64.msi /x /quiet
