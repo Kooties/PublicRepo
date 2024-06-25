d4b6d5a04aebd66c3be56b661c1c8bc4d4fa0a2a
Krystina Kyle <krystina.kyle@storable.com>
2024-06-25 13:03:25 -0400

    sync

--- BlogScripts/GCPW_Install/uninstallGCPW.ps1
+++ BlogScripts/GCPW_Install/uninstallGCPW.ps1
@@ -1,7 +1,14 @@
-Remove-Item -Path HKLM:\SOFTWARE\Google\GCPW -Force
-Remove-Item -Path 'HKLM:\SOFTWARE\WOW6432Node\Google\Update\ClientState\{430FD4D0-B729-4F61-AA34-91526481799D}' -Force
-Remove-Item -Path 'HKLM:\SOFTWARE\WOW6432Node\Google\Update\ClientState\{32987697-A14E-4B89-84D6-630D5431E831}' -Force
+Remove-Item -Path HKLM:\SOFTWARE\Google\GCPW -Force -Confirm:$false
+Remove-Item -Path 'HKLM:\SOFTWARE\WOW6432Node\Google\Update\ClientState\{430FD4D0-B729-4F61-AA34-91526481799D}' -Force -Confirm:$false
+Remove-Item -Path 'HKLM:\SOFTWARE\WOW6432Node\Google\Update\ClientState\{32987697-A14E-4B89-84D6-630D5431E831}' -Force -Confirm:$false
+
+Set-Location "C:\Program Files\Google\Credential Provider"
+$folder = Get-ChildItem
+Set-Location $folder.Name
+.\gcp_setup.exe /uninstall
 
 
 
-.\gcpwstandaloneenterprise64.msi /x /quiet
+.\gcpwstandaloneenterprise64.msi --uninstall
+
+msiexec /x gcpwstandaloneenterprise64.msi /quiet /qn
