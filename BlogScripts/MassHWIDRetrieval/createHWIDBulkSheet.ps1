deb347a17b6f3d9aa61f876c49aba76077aa328e
Krystina Kyle <krystina.kyle@storable.com>
2024-10-14 12:24:45 -0400

    additional comments

--- BlogScripts/MassHWIDRetrieval/createHWIDBulkSheet.ps1
+++ BlogScripts/MassHWIDRetrieval/createHWIDBulkSheet.ps1
@@ -1,5 +1,6 @@
 #path where you can dump all the device diagnostics downloaded via Intune
 $path = "C:\path\to\folder\with\downloaded\diagnostics"
+#directories used during processing
 $zipDump = $path + "\dump"
 $csvDump = $path + "\csvDump"
 if(!(test-path $zipDump)){
@@ -21,21 +22,22 @@ foreach($zip in $content){
     if(!(test-Path $folderName)){
         New-Item -Path $folderName -ItemType Directory
     }
+    #unzip file, then delete ZIP file
     Expand-Archive -path $zipPath -DestinationPath $folderName
     Remove-Item $zipPath -recurse -force
     $cabFolder = get-childitem $folderName | Where-Object{$_.Name -match "MDMDiagnostics_mdmlogs"}
     $cabFile = get-childitem $cabfolder.fullname
+    #grabbing the specific DeviceHash/HWID CSV from the CAB file and only extracting it
     expand.exe $cabFile.fullName -f:DeviceHash* $csvDump
+    #once the HWID has been extracted from the CAB, the unzipped Diagnostics directory is no longer needed
     Remove-Item $folderName -recurse -force
 }
+#after all of the ZIP files have been processed, they're no longer needed
 Remove-Item $zipDump -recurse -force
-#All Device Hashes should now be in their own folder, so we just need to combine them
+#All Device Hashes should now be in their own folder of only CSVs, so we just need to combine them
 
-$output = $path + "\output"
-if(!(test-path $output)){
-    New-Item -Path $output -ItemType Directory
-}
-$outCSV = $output + "\hardwareHashes.csv"
+
+$outCSV = $path + "\hardwareHashes.csv"
 
 Get-ChildItem -Path $csvDump -Filter *.csv | Select-Object -ExpandProperty FullName | Import-Csv | Export-Csv -path $outCSV -NoTypeInformation -Append
 Remove-Item $csvDump -recurse -force
