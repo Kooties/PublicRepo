332c9448c9a79386a56784f8572ff62029590762
Krystina Kyle <krystina.kyle@storable.com>
2024-10-29 09:11:13 -0400

    update

---
+++ IntuneRemediations/PythonInExcelBlock/remediate.ps1
@@ -0,0 +1,3 @@
+reg add HKCU\software\policies\microsoft\office\16.0\excel\security /v PythonFunctionWarnings /t REG_DWORD /d 2 /f
+
+reg add HKCU\software\Microsoft\Office\16.0\excel\security /v PythonFunctionWarnings /t REG_DWORD /d 2 /f
